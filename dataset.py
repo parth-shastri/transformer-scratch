Changes greedy decoding function to match that of mingpt  - Fixed bugs in the model  - fixed the attention mask in the myGPT class

dataset.py
@@ -78,7 +78,7 @@ class WikiText(Dataset):
             [
                 torch.tensor(data_tokenized, dtype=torch.int64),
                 self.eos_token,
-                torch.tensor([self.pad_token] * pad_len, dtype=torch.int64),
+                torch.tensor([-100] * pad_len, dtype=torch.int64),   # replace pad token to -100 in the label
             ],
             dim=0,
         )
@@ -118,14 +118,18 @@ if __name__ == "__main__":
 
     train_dataloader = DataLoader(train_ds, batch_size=config.batch_size)
 
-    # for data in train_dataloader:
-    #     print(data['input_ids'].shape)
+    # look at 10 examples 
+    print(train_ds[0])
+
+    # look at one batch
+    for data in train_dataloader:
+        print(data)
+        break
 
     from data_utils import data_iterator
     val_iterator = data_iterator(val_ds)
 
-    for item in val_iterator:
-        print(len(item))
-        
+    # for item in val_iterator:
+    #     print(item)   
 
     # print(len(val_ds))

mingpt.py
@@ -48,9 +48,12 @@ class CausalMultiHeadAttention(nn.Module):
         # causal attention mask
         self.register_buffer(
             "masked_bias",
-            torch.tril(torch.ones((config.seq_len, config.seq_len))
-                       .view(1, 1, config.seq_len, config.seq_len))
-            )
+            torch.tril(
+                torch.ones((config.seq_len, config.seq_len)).view(
+                    1, 1, config.seq_len, config.seq_len
+                )
+            ),
+        )
         self.n_head = config.num_heads
         self.n_embed = config.d_model
 
@@ -58,7 +61,7 @@ class CausalMultiHeadAttention(nn.Module):
         B, T, C = x.size()  # (B, seq_len, d_model)
 
         # calculate the query, key and values
-        q, k, v = self.c_attn(x).split(self.n_embed, dim=2) 
+        q, k, v = self.c_attn(x).split(self.n_embed, dim=2)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
@@ -66,35 +69,40 @@ class CausalMultiHeadAttention(nn.Module):
         # causal self attention
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
         # fill the causal mask
-        att = att.masked_fill(self.masked_bias[:, :, :T, :T] == 0, float("-inf"))  # fill with -ve inf vals
+        att = att.masked_fill(
+            self.masked_bias[:, :, :T, :T] == 0, float("-inf")
+        )  # fill with -ve inf vals
         # softmax
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
         y = att @ v
         y = y.transpose(1, 2).contiguous().view(B, T, C)
-        
+
         # output projection w_o
         y = self.residual_dropout(self.c_proj(y))
         return y
 
 
 class Block(nn.Module):
-    """ Transformer Block"""
+    """Transformer Block"""
+
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.d_model)
         self.attn = CausalMultiHeadAttention(config)
         self.ln_2 = nn.LayerNorm(config.d_model)
-        self.mlp = nn.ModuleDict(dict(
-            c_fc = nn.Linear(config.d_model, config.d_ff),
-            c_proj = nn.Linear(config.d_ff, config.d_model),
-            act = GELU(),
-            dropout = nn.Dropout(config.ff_dropout)
-        ))
+        self.mlp = nn.ModuleDict(
+            dict(
+                c_fc=nn.Linear(config.d_model, config.d_ff),
+                c_proj=nn.Linear(config.d_ff, config.d_model),
+                act=GELU(),
+                dropout=nn.Dropout(config.ff_dropout),
+            )
+        )
 
         m = self.mlp
         self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x))))
-    
+
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))  # prenorm
         x = x + self.mlpf(self.ln_2(x))
@@ -108,24 +116,28 @@ class GPT(nn.Module):
         assert config.d_model is not None
         self.seq_len = config.seq_len
 
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.d_model),
-            wpe = nn.Embedding(config.seq_len, config.d_model),
-            drop = nn.Dropout(config.embed_dropout),
-            h = nn.ModuleList([Block(config) for _ in range(config.num_layers)]),
-            ln_f = nn.LayerNorm(config.d_model)
-        ))
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.d_model),
+                wpe=nn.Embedding(config.seq_len, config.d_model),
+                drop=nn.Dropout(config.embed_dropout),
+                h=nn.ModuleList([Block(config) for _ in range(config.num_layers)]),
+                ln_f=nn.LayerNorm(config.d_model),
+            )
+        )
         self.lm_head = nn.Linear(config.d_model, config.vocab_size, bias=False)
 
         # init all the weights as normal distribution N(0 , 0.02)
         self.apply(self._init_weights)
         for pn, p in self.named_parameters():
-            if pn.endswith('c_proj.weight'):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02 / math.sqrt(2 * config.num_layers))
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.num_layers)
+                )
 
         # report number of parameters
         n_parameters = sum(p.numel() for p in self.transformer.parameters())
-        print("number of parameters: %.2fM" % (n_parameters/1e6,))
+        print("number of parameters: %.2fM" % (n_parameters / 1e6,))
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
@@ -141,8 +153,12 @@ class GPT(nn.Module):
     def forward(self, idx, targets=None):
         device = idx.device
         b, t = idx.size()
-        assert t <= self.seq_len, f"The max seq_len is exceeded, Found len {t} expected {self.seq_len}"
-        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0) # shape (1, t)
+        assert (
+            t <= self.seq_len
+        ), f"The max seq_len is exceeded, Found len {t} expected {self.seq_len}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(
+            0
+        )  # shape (1, t)
 
         # forward the model
         token_emb = self.transformer.wte(idx)
@@ -157,7 +173,9 @@ class GPT(nn.Module):
         # if the targets are given compute the loss
         loss = None
         if targets is not None:
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-100
+            )
         return logits, loss
 
     @classmethod
@@ -167,7 +185,7 @@ class GPT(nn.Module):
         match the weights and then copy them over to our little GPT
         """
 
-        assert model_name in {"gpt2", "gpt2-medium", 'gpt2-large', 'gpt2-xl'}
+        assert model_name in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
         from transformers import GPT2LMHeadModel
         from config import MyGPTConfig
 
@@ -186,9 +204,14 @@ class GPT(nn.Module):
 
         # init the huggingface/transformers model
         model_hf = GPT2LMHeadModel.from_pretrained(model_name)
-        sd_hf = model_hf.state_dict() 
+        sd_hf = model_hf.state_dict()
         keys = [k for k in sd if not k.endswith("attn.masked_bias")]
-        transposed = ["attn.c_attn.weight", "attn.c_proj.weight", "mlp.c_fc.weight", "mlp.c_proj.weight"]
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
 
         assert len(keys) == len(sd_hf)
         for k in keys:
@@ -202,7 +225,9 @@ class GPT(nn.Module):
 
             else:
                 # vanilla copy for all the others
-                assert sd_hf[k].shape == sd[k].shape, f"Shape mismatch for {k}. Found {sd_hf[k].shape} and {sd[k].shape}"
+                assert (
+                    sd_hf[k].shape == sd[k].shape
+                ), f"Shape mismatch for {k}. Found {sd_hf[k].shape} and {sd[k].shape}"
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k])
 
@@ -210,7 +235,7 @@ class GPT(nn.Module):
 
     def configure_optimizers(self, config):
         """
-        Configure optimizers for the GPT model, 
+        Configure optimizers for the GPT model,
          1. The LayerNorm should not be added to the weight decay
          2. Embedding layers wont experience weight decay
          3. Biases should be excluded as well
@@ -218,7 +243,7 @@ class GPT(nn.Module):
 
         decay = set()
         no_decay = set()
-        include_modules = (nn.Linear, )
+        include_modules = (nn.Linear,)
         exclude_modules = (nn.LayerNorm, nn.Embedding)
 
         for mn, m in self.named_modules():
@@ -229,7 +254,7 @@ class GPT(nn.Module):
                 if pn.endswith("bias"):
                     # all biases are excluded
                     no_decay.add(full_name)
-                elif pn.endswith('weight') and isinstance(m, include_modules):
+                elif pn.endswith("weight") and isinstance(m, include_modules):
                     decay.add(full_name)
                 elif pn.endswith("weight") and isinstance(m, exclude_modules):
                     no_decay.add(full_name)
@@ -240,29 +265,33 @@ class GPT(nn.Module):
         union_params = decay | no_decay
 
         assert len(inter_params) == 0
-        assert len(param_dict.keys() - union_params) == 0 
+        assert len(param_dict.keys() - union_params) == 0
 
         optim_params = [
             {
-                "params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": config.weight_decay
+                "params": [param_dict[pn] for pn in sorted(list(decay))],
+                "weight_decay": config.weight_decay,
             },
             {
-                "params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0
-            }
-        ] 
+                "params": [param_dict[pn] for pn in sorted(list(no_decay))],
+                "weight_decay": 0.0,
+            },
+        ]
         optimizer = torch.optim.AdamW(optim_params, lr=config.lr, betas=config.betas)
 
-        return optimizer              
+        return optimizer
 
     @torch.no_grad
-    def generate(self, idx, max_new_tokens, temperature=0.1, do_sample=True, top_k=None):
+    def generate(
+        self, idx, max_new_tokens, temperature=0.1, do_sample=True, top_k=None
+    ):
         """
         This is the function to generate max_new_tokens len of tokens given idx (LongTensor of shape (b, seq_len))
         either greedy or sampling from a distribution, with top_k, use in model.eval()
         """
         for _ in range(max_new_tokens):
             # if the input sequence is too long crop it from the left
-            idx_cond = idx if idx.size(1) <= self.seq_len else idx[:, :-self.seq_len]
+            idx_cond = idx if idx.size(1) <= self.seq_len else idx[:, : -self.seq_len]
             # forward pass through the model
             logits, _ = self(idx_cond)
             # take the last logit

model_utils.py
@@ -61,13 +61,16 @@ class InputEmbeddings(nn.Module):
     
 
 class MultiHeadAttentionBlock(nn.Module):
-    def __init__(self, d_model: int, h: int, dropout: float):
+    def __init__(self, d_model: int, seq_len: int, h: int, dropout: float):
         super().__init__()
         self.d_model = d_model
         self.h = h
+        self.seq_len = seq_len
         # make sure the d_model is divisible by h
         assert d_model % h == 0, 'd_model is not divisible by h'
         # initilalize the projection layers to generate the q, k & v
+
+        self.register_buffer("masked_bias", torch.tril(torch.ones([self.seq_len, self.seq_len])).unsqueeze(0).unsqueeze(0))
         
         self.d_k = self.d_model // h # the per head feature dim
         self.w_q = nn.Linear(d_model, d_model, bias=False)
@@ -76,17 +79,16 @@ class MultiHeadAttentionBlock(nn.Module):
         self.w_o = nn.Linear(d_model, d_model, bias=False)
         self.dropout = nn.Dropout(dropout)
 
-    @staticmethod
-    def attention(q, k, v, mask, dropout: nn.Dropout):
-        d_k = q.shape[-1]
+    def attention(self, q, k, v, dropout: nn.Dropout):
+        _, _, T, d_k = q.shape
         # formula from the paper attention is all you need
         # (B, h, seq_len, d_k) ---> (b, h, seq_len, seq_len)
         # attention_scores = (q @ k.transpose(-2, -1) / math.sqrt(d_k))
         attention_scores = einsum(q, k, "b h i d, b h j d -> b h i j",) / math.sqrt(d_k)
-        if mask is not None:
+        if self.masked_bias is not None:
             # using the masked_fill_ method of the tensor
-            # write very small values where mask is zero to denote -inf -1e9
-            attention_scores.masked_fill_(mask, -1e9)
+            # write very small values where mask is zero to denote -inf ==> -1e9
+            attention_scores.masked_fill_(self.masked_bias[:, :, :T, :T] == 0, -1e9)
         attention_scores = attention_scores.softmax(dim=-1)
         if dropout is not None:
             attention_scores = dropout(attention_scores)
@@ -95,10 +97,10 @@ class MultiHeadAttentionBlock(nn.Module):
         # return the attention scores so that they can be visualized
         return (attention_scores @ v), attention_scores
 
-    def forward(self, q, k, v, mask):
+    def forward(self, q, k, v):
         query = self.w_q(q)
         key = self.w_k(k)
-        value = self.w_q(q)
+        value = self.w_q(v)
 
         # (B, seq_len, d_model) --> (B, seq_len, h, d_k) --> (B, h, seq_len, d_k)
         query = rearrange(query.reshape(query.shape[0], query.shape[1], self.h, self.d_k), 'b s h d -> b h s d')
@@ -106,7 +108,7 @@ class MultiHeadAttentionBlock(nn.Module):
         value = rearrange(value.reshape(value.shape[0], value.shape[1], self.h, self.d_k), 'b s h d -> b h s d')
 
         # calculate the attention
-        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask , self.dropout)
+        x, self.attention_scores = self.attention(query, key, value, self.dropout)
 
         # combine all the heads
         #  (B, h, seq_len, d_k) --> (B, seq_len, h, d_k) --> (B, seq_len, d_model)
@@ -144,10 +146,10 @@ class FeedForwardBlock(nn.Module):
     
     
 class DecoderBlock(nn.Module):
-    def __init__(self, d_model: int, h: int, d_ff:int, mha_dropout: float, ff_dropout: float, res_dropout: float, prenorm=True):
+    def __init__(self, d_model: int, seq_len: int, h: int, d_ff:int, mha_dropout: float, ff_dropout: float, res_dropout: float, prenorm=True):
         super().__init__()
         # init the multihead attention and the feedforward block
-        self.mha = MultiHeadAttentionBlock(d_model, h, mha_dropout)
+        self.mha = MultiHeadAttentionBlock(d_model, seq_len, h, mha_dropout)
         # init the residual connections
         self.res_1 = ResidualConnection(d_model, res_dropout, prenorm=True)
         self.res_2 = ResidualConnection(d_model, res_dropout, prenorm=True)
@@ -155,9 +157,9 @@ class DecoderBlock(nn.Module):
         # init the feedforward connection
         self.ff = FeedForwardBlock(d_ff, d_model, ff_dropout)
 
-    def forward(self, x, attn_mask):
+    def forward(self, x):
         # (B, seq_len, d_model)
-        x = self.res_1(x, lambda x: self.mha(x, x, x, attn_mask))
+        x = self.res_1(x, lambda x: self.mha(x, x, x))
         x = self.res_2(x, self.ff)
         return x
 
@@ -170,17 +172,17 @@ class Decoder(nn.Module):
         # init the positional encoding layer
         self.pos_enc = PositionalEncoding(d_model, seq_len, res_dropout)
         # init the layers with the decoder block
-        decoder_blocks = [DecoderBlock(d_model, h, d_ff, mha_dropout, ff_dropout, res_dropout, prenorm) for _ in range(n_layers)]
+        decoder_blocks = [DecoderBlock(d_model, seq_len, h, d_ff, mha_dropout, ff_dropout, res_dropout, prenorm) for _ in range(n_layers)]
 
         self.decoder_blocks = nn.ModuleList(decoder_blocks)
     
-    def forward(self, x, attn_mask):
+    def forward(self, x):
         # (B, seq_len) --> (B, seq_len, d_model)
         x = self.emb(x)
         x = self.pos_enc(x)
 
         for sublayer in self.decoder_blocks:
-            x = sublayer(x, attn_mask)
+            x = sublayer(x)
         
         return x
 

mygpt.py
@@ -1,5 +1,6 @@
 import torch 
 import torch.nn as nn
+import torch.nn.functional as F
 from model_utils import Decoder, ProjectionLayer
 
 class MyGPT(nn.Module):
@@ -9,10 +10,15 @@ class MyGPT(nn.Module):
         self.decoder = Decoder(n_layers, seq_len, vocab_size, d_model, h, d_ff, mha_dropout, ff_dropout, res_dropout, prenorm)
         self.proj = ProjectionLayer(d_model, vocab_size)
 
-    def forward(self, x: torch.Tensor, attn_mask) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, targets=None) -> torch.Tensor:
         # (B, seq_len, 1) --> (B, seq_len, vocab_size)
-        x = self.decoder(x, attn_mask)
-        return self.proj(x)
+        x = self.decoder(x)
+        logits = self.proj(x)
+
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-100)
+        return logits, loss
 
 
 def build_gpt(vocab_size, seq_len, n_layers: int=6, d_model: int=512, mha_dropout: float=0.1, ff_dropout: int=0.1, res_dropout: float=0.1, h:int=8, d_ff: int=2048, prenorm=True) -> MyGPT:
@@ -37,7 +43,9 @@ def _test_model_init():
     print("test seq_len: {}".format(seq_len))
     print("test batch_size: {}".format(batch_size))
 
-    out = myGPT(x, torch.triu(torch.ones((1, seq_len, seq_len)), diagonal=1)[:, None, ...].type(torch.int) == 0)
+    #mask simulation
+    mask = torch.triu(torch.ones((1, seq_len, seq_len)), diagonal=1)[:, None, ...].type(torch.int) == 0
+    out, _ = myGPT(x)
     print("Output shape: {}".format(out.shape))
 
 

train.py
@@ -44,41 +44,21 @@ def greedy_decoding(
     eos_idx = tokenizer.token_to_id(config.eos_token)
 
     # Init the decoder input
-    decoder_input = (
-        torch.empty(1, 1)
-        .fill_(sos_idx)
-        .type_as(context if context is not None else torch.int64)
-    )  # (1, 1)
-    if context is not None:
-        decoder_input = torch.cat([decoder_input, context], dim=1)
+    decoder_input = context if context is not None else torch.tensor([sos_idx], dtype=torch.int64).unsqueeze(0)
 
     # the decoding loop
     while True:
         if decoder_input.size(1) == max_len:
             break
 
-        # build decoder mask
-        decoder_mask = (
-            causal_mask(decoder_input.size(1)).unsqueeze(1).to(device)
-        )  # (1, 1, context_len, context_len)
-
         # calculate the output
-        out = model(decoder_input, decoder_mask)
+        out, _ = model(decoder_input)
 
         # probs of the next token
         prob = F.softmax(out[:, -1, :], dim=-1)
-        print(prob)
+        # greedy decode (select the argmax)
         _, next_word = torch.max(prob, dim=-1)
-        decoder_input = torch.cat(
-            [
-                decoder_input,
-                torch.empty(1, 1)
-                .fill_(next_word.item())
-                .type_as(decoder_input)
-                .to(device),
-            ],
-            dim=1,
-        )
+        decoder_input = torch.cat([decoder_input, next_word.unsqueeze(-1)], dim=1)
 
         if next_word == eos_idx:
             break
@@ -247,10 +227,10 @@ def train(config: MyGPTConfig):
     # PRELOAD AND INIT #
     ####################
     # summary writer
-    writer = SummaryWriter(
-        config.experiment_name,
-        comment=f"LR_{config.lr}_BATCH_{config.batch_size}_ADAMW",
-    )
+    # writer = SummaryWriter(
+    #     config.experiment_name,
+    #     comment=f"LR_{config.lr}_BATCH_{config.batch_size}_ADAMW",
+    # )
     # weight loading
     initial_epoch = 0
     global_step = 0
@@ -293,42 +273,42 @@ def train(config: MyGPTConfig):
         ##############
         # TRAIN LOOP #
         ##############
-        for batch_idx, batch in enumerate(batch_iter):
-            model_input = batch["input_ids"].to(device)  # (B, seq_len)
-            attention_mask = batch[
-                "attention_mask"
-            ]  # (B, 1, seq_len) dont put this on GPU as this is not to be used
+        # for batch_idx, batch in enumerate(batch_iter):
+        #     model_input = batch["input_ids"].to(device)  # (B, seq_len)
+        #     attention_mask = batch[
+        #         "attention_mask"
+        #     ]  # (B, 1, seq_len) dont put this on GPU as this is not to be used
 
-            # label
-            label = batch["labels"].to(device)  # (B, seq_len)
+        #     # label
+        #     label = batch["labels"].to(device)  # (B, seq_len)
 
-            # cau_msk = causal_mask(attention_mask.size(-1))  # (1, seq_len, seq_len)
-            # decoder_mask = (attention_mask & cau_msk)[:, None, ...].to(
-            #     device
-            # )  # mask out the padding token and the next token (B, 1, seq_len, seq_len)
+        #     # cau_msk = causal_mask(attention_mask.size(-1))  # (1, seq_len, seq_len)
+        #     # decoder_mask = (attention_mask & cau_msk)[:, None, ...].to(
+        #     #     device
+        #     # )  # mask out the padding token and the next token (B, 1, seq_len, seq_len)
 
-            # output
-            model_out, _ = model(model_input)
+        #     # output
+        #     model_out, _ = model(model_input)
 
-            # Calculate theloss
-            loss = loss_fn(model_out.view(-1, config.vocab_size), label.view(-1))
-            # modify the loss for the accumulation steps
-            loss = loss / config.accumulation_steps
-            batch_iter.set_postfix({"loss": f"{loss.item():6.3f}"})
+        #     # Calculate theloss
+        #     loss = loss_fn(model_out.view(-1, config.vocab_size), label.view(-1), ignore_index=-100)
+        #     # modify the loss for the accumulation steps
+        #     loss = loss / config.accumulation_steps
+        #     batch_iter.set_postfix({"loss": f"{loss.item():6.3f}"})
 
-            # Log the loss
-            writer.add_scalar("train_loss", loss.item(), global_step)
-            writer.flush()
+        #     # Log the loss
+        #     writer.add_scalar("train_loss", loss.item(), global_step)
+        #     writer.flush()
 
-            # Backpropagate the gradients
-            loss.backward()
+        #     # Backpropagate the gradients
+        #     loss.backward()
 
-            # Update the weights according to the gradient accumulation steps
-            if not batch_idx % config.accumulation_steps:
-                optimizer.step()
-                optimizer.zero_grad(set_to_none=True)
+        #     # Update the weights according to the gradient accumulation steps
+        #     if not batch_idx % config.accumulation_steps:
+        #         optimizer.step()
+        #         optimizer.zero_grad(set_to_none=True)
 
-            global_step += 1
+        #     global_step += 1
 
         # Run validation
         writer = None